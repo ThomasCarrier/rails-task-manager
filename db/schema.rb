655b6c74e2a4fdcf4021463d236b0282e6432b6a
Thomas Carrier <hthomascarrier@gmail.com>
2016-08-02 14:43:47 +0100

    added Owner to tasks db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160802133557) do
+ActiveRecord::Schema.define(version: 20160802134125) do
 
   create_table "tasks", force: :cascade do |t|
     t.string   "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20160802133557) do
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "owner"
   end
 
 end
